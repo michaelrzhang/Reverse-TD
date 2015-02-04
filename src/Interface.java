fbeeedcc96693fb7d9d2b024ea2bd4bf995da82a
avillaflor <avillaflor@berkeley.edu>
2015-02-03 23:43:06 -0800

    added Shape and Interface

--- src/Interface.java
+++ src/Interface.java
@@ -1,11 +1,8 @@
-public abstract class Tower{
+public abstract class UI{
 	double x_position;
 	double y_position;
 	String name;
 	Shape hit_box;
-	int cost;
-	int health;
-	abstract void level_up();
 	abstract void action(Creature[] creatures);
 	abstract void draw();
 }
---
+++ src/Shape.java
@@ -0,0 +1,6 @@
+public abstract Shape class{
+	double x_position;
+	double y_position;
+	String name;
+	abstract boolean is_overlapped(Shape s);
+}
