ab7d39595f84834d641d0734f474906e0a7a8e92
Michael Zhang <michaelzhang333@gmail.com>
2015-02-05 16:48:30 -0800

    Launcher class

---
+++ src/Launcher.java
@@ -0,0 +1,9 @@
+public class Launcher{
+	// perhaps have a test class for testing smaller amounts of the code
+	// good article on mechanics to include in game: http://www.emanueleferonato.com/2007/10/06/make-a-flash-game-like-flash-element-tower-defense-part-1/
+	public static void main(String[] args){
+	// maybe break down by step and test each part?
+	// initiate game method?
+	// move_time method?
+	}
+}
